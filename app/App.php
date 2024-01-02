cba67715178893075dc7ae163edf2f3a91b616b9
José Manuel Cornelio <jose@cornelioweb.com>
2024-01-02 17:12:15 -0400

    Print raw transactions

--- app/App.php
+++ app/App.php
@@ -3,3 +3,35 @@
 declare(strict_types = 1);
 
 // Your Code
+function getTransactionFiles(string $dirPath): array 
+{
+    $files = [];
+    foreach(scandir($dirPath) as $file) {
+        //Filter the directories and just bring the files
+        if (is_dir($file)) {
+            continue;
+        }
+        //Full path to the file
+        $files[] = $dirPath . $file;
+    }
+    return $files;
+}
+
+function getTransactions(string $fileName): array
+{
+    if (!file_exists($fileName)) {
+        trigger_error('File do not exists', E_USER_ERROR);
+    }
+
+    $file = fopen($fileName, 'r');
+    $transactions = [];
+
+    //Ignore the first line (file headers)
+    $transaction = fgetcsv($file);
+
+    while (($transaction = fgetcsv($file)) !== false) {
+        $transactions[] = $transaction;
+    }
+
+    return $transactions;
+}
